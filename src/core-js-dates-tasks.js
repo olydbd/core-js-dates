a05cff4c86f5ed4a50893100becd6efef71aa2b6
Olya Dubodel <dubodel.olya@gmail.com>
2025-03-21 17:21:26 +0300

    feat: solve one task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -269,8 +269,37 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
-  throw new Error('Not implemented');
+
+function refactorDate(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  const dateString = `${day}-${month}-${year}`;
+
+  return dateString;
+}
+
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  const startDate = new Date(period.start.split('-').reverse().join('-'));
+  const endDate = new Date(period.end.split('-').reverse().join('-'));
+
+  let countWorkDaysCopy = countWorkDays;
+
+  const res = [];
+
+  while (startDate <= endDate) {
+    res.push(refactorDate(startDate));
+    countWorkDaysCopy -= 1;
+    if (!countWorkDaysCopy) {
+      startDate.setDate(startDate.getDate() + countOffDays + 1);
+      countWorkDaysCopy = countWorkDays;
+    } else {
+      startDate.setDate(startDate.getDate() + 1);
+    }
+  }
+
+  return res;
 }
 
 /**
